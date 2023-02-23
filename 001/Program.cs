f05c81f4a17f9e8a3d76384451fbb0b39027920b
Your Name <you@example.com>
2023-02-23 21:36:54 +0500

    Lesson 1 02

--- 001/Program.cs
+++ 001/Program.cs
@@ -1,2 +1,6 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// 1.С клавиатуры вводится целое число. Вывести квадрат числа 
+    string s;
+    int a;
+    s=Console.ReadLine();
+    a=Convert.ToInt32(s);
+    System.Console.WriteLine(a*a); 
---
+++ 003/Program.cs
@@ -0,0 +1,13 @@
+﻿//С клавиатуры вводятся два числа a и b. Найти максимальное из них. 
+int a,b;
+string s;
+System.Console.WriteLine("Введите a:");
+s=Console.ReadLine();
+a=Convert.ToInt32(s);
+System.Console.WriteLine("Введите b:");
+s=Console.ReadLine();
+b=Convert.ToInt32(s);
+if (a>b)
+    System.Console.WriteLine("a - максимальное");
+else 
+    System.Console.WriteLine("b - максимальное"); 
